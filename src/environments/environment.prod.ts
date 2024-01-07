bbac71a380c7fdf03c80baaa8780e17b817bb124
Elmer Ramos <35051125+elmer-rl@users.noreply.github.com>
2024-01-07 13:55:06 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDI6jYGdG1a6QtJ35Igy_mPsH0eRAgAgII",
-    authDomain: "chat-706d3.firebaseapp.com",
-    projectId: "chat-706d3",
-    storageBucket: "chat-706d3.appspot.com",
-    messagingSenderId: "616999997119",
-    appId: "1:616999997119:web:09fdcbad03d0c6c9ebc00a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 }
